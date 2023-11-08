c4b20091e7c9c662c3bc5c346b2653adeaea571c
Carlos <carlos.dimitrakis@liceocristiano.edu.ec>
2023-11-07 21:11:14 -0500

    Segundo Cambios

--- RegistroEstudiate/MainPage.xaml.cs
+++ RegistroEstudiate/MainPage.xaml.cs
@@ -3,6 +3,7 @@
 
     public partial class MainPage : ContentPage
     {
+        private string titulo = "Registro";
         public Estudiante MiEstudiante { get; set; }
         public MainPage()
         {
@@ -10,9 +11,34 @@
             MiEstudiante = new Estudiante();
             BindingContext = MiEstudiante;
         }
+
         private void Confirmar_Clicked(object sender, EventArgs e)
         {
-            DisplayAlert("Registro ", $"{MiEstudiante} Sus datos se ingresaron correctamente", "ok");
+            if (IsValid())
+            {
+                var messaje = $"{MiEstudiante.Nombre} sus datos se ingresaron correctamente";
+                DisplayAlert(titulo, messaje, "ok");
+            }     
+        }
+
+        private bool IsValid()
+        {
+            if (String.IsNullOrEmpty(MiEstudiante.Nombre)) 
+            {
+                DisplayAlert(titulo, "ERROR CAMPO DEL NOMBRE VACIOS", "ok");
+                return false;
+            }
+            if (String.IsNullOrEmpty(MiEstudiante.Apellido))
+            {
+                DisplayAlert(titulo, "ERROR CAMPO DEL APELLIDO VACIOS", "ok");
+                return false;
+            }
+            if (String.IsNullOrEmpty(MiEstudiante.Materia))
+            {
+                DisplayAlert(titulo, "ERROR CAMPO DEL MATERIA VACIOS", "ok");
+                return false;
+            }
+            return true;
         }
     }
 }
